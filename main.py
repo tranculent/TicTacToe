2308f08006c703d40f7c79378b9e768530d4699b
Yanko Mirov <45804656+tranculent@users.noreply.github.com>
2021-04-18 17:14:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+from tkinter import *
+import tkinter.messagebox
+
+from Modes import ComputerMode
+import Constants
+
+class Configuration(): # Possibletter parameters modification
+    pass
